ba83d389b460f2f5fbd0b903d1505046e711a14b
luxolo <luxolo@projectcodex.co>
2016-03-30 11:08:32 +0200

    fourth function

--- number_list.js
+++ number_list.js
@@ -1,6 +1,8 @@
-module.exports = function(n){
+//creating a function
+module.exports = function(){
+  //creating a variable
   var number_list = [1,2,3,4,5];
+  // return number list
   return number_list;
-
 
 };
