7b3fec5cba9c819e1ef20c7be6ccbcf9b2e54f7b
luxolo <luxolo@projectcodex.co>
2016-03-30 10:18:32 +0200

    second function

--- hello_uppercase.js
+++ hello_uppercase.js
@@ -1,3 +1,10 @@
-module.exports = function(){
+module.exports = function(username){
+  
+if(username === "Xolani"){
 
-};
+   return "Hello," +" "+username.toUpperCase()+"!";
+} else{
+   return"Hello!".toUpperCase();
+
+}
+ };
