1cd0839bab1b3e9c84fbde9c56f8d40c84dd1f60
luxolo <luxolo@projectcodex.co>
2016-03-30 16:27:33 +0200

    fifth function

--- sum_numbers.js
+++ sum_numbers.js
@@ -1,3 +1,9 @@
-module.exports = function(){
-    
-}
+module.exports = function(n){
+  //creating variable of 0
+var sum_numbers = 0;
+//creating a for loop for both 0 n 15
+for(j=1; j<=n; j++){
+sum_numbers+=j;
+};
+return sum_numbers;
+};
