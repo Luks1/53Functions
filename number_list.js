815f1298a4b75b46136e2b01a16f77f441858031
luxolo <luxolo@projectcodex.co>
2016-03-30 13:22:52 +0200

    fourth function

--- number_list.js
+++ number_list.js
@@ -1,16 +1,25 @@
 //creating a function
-module.exports = function(n){
-  //creating an empty array for number_list
-var number_list = [];
-//creating a for loop for the number_list
- for(j=1; j<=n; j++){
-//pushing my number_list
-   number_list.push(j);
 
+module.exports = function(n){
+
+  //creating an empty array for number_list
+
+var number_list = [];
+
+//creating a for loop for the number_list
+
+ for(j=1; j<=n; j++){
+
+//pushing my number_list
+
+   number_list.push(j);
 
 };
 //creating console.log to show numbers in array
+
 console.log(number_list);
+
 //returning number_list to pass my test
+
 return number_list;
 };
